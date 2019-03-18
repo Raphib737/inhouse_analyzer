c025b04e4ad62f3e09d197b5dec5a840e0420a0b
Raphib737 <raph737@bu.edu>
2019-03-17 21:11:21 -0400

    ADD: adding for heroku push

--- server.js
+++ server.js
@@ -2,11 +2,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
 const port = process.env.PORT || 5000;
+const cors = require('cors');
+
+app.unsubscribe(cors());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: true}));
 
 var overviewSeason2Data = require('./overview_data/season_2.json');
 var overviewSeason1Data = require('./overview_data/season_1.json');
+
+app.use(express.static(path.join(__dirname, 'client/build')));
 
 // var overviewSeason1Data = require('./overview_data/season_1.json');
 app.get('/api/season/2/overview', (req, res) => {

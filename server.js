629b4e43c0560f6e5062f7f62280ee0edfa15066
Michael Gerakis <mgerakis@bu.edu>
2019-10-12 22:21:57 -0400

    Update server.js to handle routing

--- server.js
+++ server.js
@@ -10,23 +10,9 @@ app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
 
 var overviewSeason3Data = require('./overview_data/season_3.json')
-var overviewSeason2Data = require('./overview_data/season_2.json');
+    var overviewSeason2Data = require('./overview_data/season_2.json');
 var overviewSeason1Data = require('./overview_data/season_1.json');
 var overviewOverallData = require('./overview_data/overall.json')
-
-// production mode
-if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join(__dirname, 'build')));
-
-  app.get('/', (req, res) => {
-    res.sendfile(path.join(__dirname = 'build/index.html'));
-  })
-}
-else {
-  app.get('/', (req, res) => {
-    res.sendfile(path.join(__dirname = 'public/index.html'));
-  });
-}
 
 app.get('/api/season/3', (req, res) => {
   res.send(overviewSeason3Data);
@@ -43,5 +29,18 @@ app.get('/api/season/1', (req, res) => {
 app.get('/api/season/overall', (req, res) => {
   res.send(overviewOverallData);
 });
+
+// production mode
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static(path.join(__dirname, 'build')));
+
+  app.get('/*', (req, res) => {
+    res.sendfile(path.join(__dirname = 'build/index.html'));
+  })
+} else {
+  app.get('/*', (req, res) => {
+    res.sendfile(path.join(__dirname = 'public/index.html'));
+  });
+}
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
